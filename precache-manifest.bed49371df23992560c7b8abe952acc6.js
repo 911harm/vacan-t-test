3a1cca204590da4b61d521765a2175a8234da55a
911harm <911harm@gmail.com>
2020-09-27 13:59:55 -0500

    Updates

---
+++ precache-manifest.bed49371df23992560c7b8abe952acc6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bdd7f88baaf2c0d53103591cf0f5e6c",
+    "url": "/vacan-t-test/index.html"
+  },
+  {
+    "revision": "fe888f62031666f96396",
+    "url": "/vacan-t-test/static/css/main.4195f5c9.chunk.css"
+  },
+  {
+    "revision": "ccfc55a5788d77e302b4",
+    "url": "/vacan-t-test/static/js/2.229994c8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/vacan-t-test/static/js/2.229994c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe888f62031666f96396",
+    "url": "/vacan-t-test/static/js/main.44048ccb.chunk.js"
+  },
+  {
+    "revision": "160ba6fad7fdf4a6620a",
+    "url": "/vacan-t-test/static/js/runtime-main.06755bb9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/vacan-t-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
